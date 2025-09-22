df764a97bb9ec8d0af483acd875550926febf4ce
Sudeep <sudeep.rp@gmail.com>
2025-09-22 17:10:53 +0530

    with step comments

--- test/cooling_alert.test.mjs
+++ test/cooling_alert.test.mjs
@@ -9,26 +9,29 @@ import { batteryTemperatureToAction,
 //   - Input parameters data-model
 //   - Expected output data-model
 
-describe("Passive cooling alert", () => {
-  it("should trigger an alert when temperature exceeds threshold", () => {
-    const action = batteryTemperatureToAction(PASSIVE_COOLING, 45.0);
-    expect(action.actionType).equals(ALERT_EMAIL);
-    expect(action.actionBody).equals('Temperature alert: 45F');
-  });
-  it("should not trigger an alert when temperature is within threshold", () => {
-    const action = batteryTemperatureToAction(PASSIVE_COOLING, 35.0);
-    expect(action.actionType).equals(NO_ACTION);
-  });
-});
+// describe("Passive cooling alert", () => {
+//   it("should trigger an alert when temperature exceeds threshold", () => {
+//     const action = batteryTemperatureToAction(PASSIVE_COOLING, 45.0);
+//     expect(action.actionType).equals(ALERT_EMAIL);
+//     expect(action.actionBody).equals('Temperature alert: 45F');
+//   });
+//   it("should not trigger an alert when temperature is within threshold", () => {
+//     const action = batteryTemperatureToAction(PASSIVE_COOLING, 35.0);
+//     expect(action.actionType).equals(NO_ACTION);
+//   });
+// });
 
 // ----------- STEP 2 code ends -------
 
 // ----------- STEP 4 -----------------
 // Add the next straight-forward test: active cooling type
-describe("Active cooling alert", () => {
-  it("should not trigger an alert when temperature is within threshold", () => {
-    const action = batteryTemperatureToAction(ACTIVE_COOLING, 65.0);
-    expect(action.actionType).equals(ALERT_EMAIL);
-    expect(action.actionBody).equals('Temperature alert: 65F');
-  });
-});
+
+// describe("Active cooling alert", () => {
+//   it("should not trigger an alert when temperature is within threshold", () => {
+//     const action = batteryTemperatureToAction(ACTIVE_COOLING, 65.0);
+//     expect(action.actionType).equals(ALERT_EMAIL);
+//     expect(action.actionBody).equals('Temperature alert: 65F');
+//   });
+// });
+
+// ----------- STEP 4 code ends -------
